d457de1f33c78acd1c7a666547aae19575e373c6
hod0ri <minthod0ri@gmail.com>
2024-02-09 02:22:44 +0900

    [ Auth ] 로그인 구현

--- Canteen/user/urls.py
+++ Canteen/user/urls.py
@@ -1,6 +1,7 @@
-from user.views import RegisterAPIView
+from user.views import RegisterAPIView, AuthAPIView
 from django.urls import path
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()),
+    path("auth/", AuthAPIView.as_view()),
 ]

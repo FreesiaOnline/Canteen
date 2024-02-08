32165f801d4f8cadd7d2a2eed74aba3c08b72cd8
hod0ri <minthod0ri@gmail.com>
2024-02-09 02:26:53 +0900

    [ Auth ] 토큰 재발급 구현

--- Canteen/user/urls.py
+++ Canteen/user/urls.py
@@ -1,7 +1,9 @@
 from user.views import RegisterAPIView, AuthAPIView
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()),
     path("auth/", AuthAPIView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
 ]

d3a04f6990c659f346fb13b12415ded0c4f90dbf
hod0ri <minthod0ri@gmail.com>
2024-02-09 12:09:37 +0900

    [ CORS ] 모든 도메인 cors 허용

--- Canteen/Canteen/settings.py
+++ Canteen/Canteen/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'user'
 ]
 
@@ -85,6 +86,8 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -173,3 +176,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 APPEND_SLASH=False
+
+CORS_ORIGIN_ALLOW_ALL = True
